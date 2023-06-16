c3e32c8fbe1c3b6dadee3604380d888f786028a3
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-06-17 00:30:47 +0100

    Icon added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContactoComponent } from './contacto/contacto.component';
 import { ContactoService } from './contacto.service';
-import {MatToolbarModule} from '@angular/material/toolbar';
+
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatTableModule} from '@angular/material/table';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import {FormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+
 
 
 @NgModule({
@@ -33,6 +36,7 @@ import { HttpClientModule } from '@angular/common/http';
     ReactiveFormsModule,
     MatFormFieldModule,
     MatTableModule,
+    MatTabsModule,
     FormsModule,
     BrowserAnimationsModule
   ],
